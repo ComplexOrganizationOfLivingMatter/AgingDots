5ae213ed23ffdc90e92b41af9894922d4c3ece15
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-08-31 17:45:21 +0200

    calculating nodes in cluster for the random images

--- pipelineBrdU.m
+++ pipelineBrdU.m
@@ -2,7 +2,21 @@ function [ ] = pipelineBrdU( )
 %PIPELINE Summary of this function goes here
 %   Detailed explanation goes here
     addpath(genpath('src'))
+    
     calculateDistanceMatrixNSCsBrdU
+    %% Calculated only for 3 months
     randomPickBrdU
+    
+    resultsPath = 'resultsByAnimal\NSCs BrdU\randomElection\';
+    dirAnimals = dir(resultsPath);
+    for numAnimal = 3:size(dirAnimals, 1)
+        animalDir = strcat(resultsPath, dirAnimals(numAnimal).name);
+        dirImages = dir(animalDir);
+        for numImage = 3:size(dirImages, 1)
+            randomFiles = getAllFiles(strcat(animalDir, '\', dirImages(numImage).name));
+            randomFiles = randomFiles(3:end);
+            [ meanNNodesClusterRandom20mc, stdNNodesClusterRandom20mc, meanNNodesClusterRandom50mc, stdNNodesClusterRandom50mc] = calculateNodesInCluster(randomFiles);
+        end
+    end
 end
 
