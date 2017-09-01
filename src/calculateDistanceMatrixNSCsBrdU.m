a1ec68c7ab727c22b5b69029532892e1f688a7d0
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-09-01 15:15:34 +0200

    semiautomatic tool

--- src/calculateDistanceMatrixNSCsBrdU.m
+++ src/calculateDistanceMatrixNSCsBrdU.m
@@ -3,7 +3,9 @@ function [  ] = calculateDistanceMatrixNSCsBrdU(  )
     %load all paths (former raw images distanceMatrices and new images alpha & omega)
     centroidPathFiles = 'results\distanceMatrix\rawImages\';
     oldPointsFiles = getAllFiles(centroidPathFiles);
-    newPointsFiles = getAllFiles('data\sortedByAnimal\NSCs BrdU\');
+    oldImagesPath = 'data\sortedByAnimal\total NSCs\3m';
+    oldImagesFiles = getAllFiles(oldImagesPath);
+    newPointsFiles = getAllFiles('data\sortedByAnimal\NSCs BrdU\3m NSCs BrdU');
     splittedPath = cellfun(@(x) strsplit(x, '\'), newPointsFiles, 'UniformOutput', false);
     for nFile = 1:length(newPointsFiles)
         %load raw image NSCs BrdU
@@ -20,7 +22,8 @@ function [  ] = calculateDistanceMatrixNSCsBrdU(  )
         BWerode=imerode(BW,[1,1,1;1,1,1;1,1,1]);
         BWerode=bwareaopen(BWerode,4);
         BWdilate=imdilate(BWerode,[1,1,1;1,1,1;1,1,1]);
-        figure;imshow(BWdilate)
+        figure('visible', 'off');
+        imshow(BWdilate)
 
         %load invalid area from Img
         if strcmp(imgName(1),'1')
@@ -38,38 +41,62 @@ function [  ] = calculateDistanceMatrixNSCsBrdU(  )
         centroidsNSCsBrdU=regionprops(BWdilate,'Centroid');
         centroidsNSCsBrdU=cat(1,centroidsNSCsBrdU.Centroid);
         
-        if isempty(centroidsNSCsBrdU) == 0
+        thisSplittedPath=splittedPath{nFile};
+        outputFile = ['resultsByAnimal\NSCs BrdU\distanceMatrix\'  month '\' thisSplittedPath{end-1} '\' imgName '.mat'];
+        
+        if isempty(centroidsNSCsBrdU) == 0 && exist(outputFile, 'file') ~= 2
+            imgNameAndExt = strcat(imgName, '.bmp');
+            oldImgFile = oldImagesFiles{cellfun(@(x) isempty(strfind(x, imgNameAndExt)) == 0, oldImagesFiles)};
+            
             imgNameAndExt = strcat(imgName, '.mat');
-
             oldPointsFile = oldPointsFiles{cellfun(@(x) isempty(strfind(x, imgNameAndExt)) == 0, oldPointsFiles)};
             oldPointsInfo = importdata(oldPointsFile);
 
+            for numNewCentroid = 1:size(centroidsNSCsBrdU, 1)
+                oldImg = imread(oldImgFile);
+                oldImg(:, :, 1) = 0;
+                rawImg(:, :, 2) = 0;
+                imgToShow = rawImg + oldImg;
+                h = figure; imshow(imgToShow)
+                hold on;
+                plot(centroidsNSCsBrdU(numNewCentroid, 1), centroidsNSCsBrdU(numNewCentroid, 2), 'o');
+                [x, y] = getpts(h);
+                
+                centroidsNSCsBrdU(numNewCentroid, :) = [x y];
+                close all
+            end
+            
             distanceToOldCentroids = pdist2(centroidsNSCsBrdU, oldPointsInfo.Centroids);
-
             [~, indices] = min(distanceToOldCentroids, [], 2);
 
             centroidsNSCsBrdU = oldPointsInfo.Centroids(indices, :);
+            
+%             figure; imshow(oldImgFile)
+%             hold on;
+%             for numNewCentroid = 1:size(centroidsNSCsBrdU, 1)
+%                 plot(centroidsNSCsBrdU(numNewCentroid, 1), centroidsNSCsBrdU(numNewCentroid, 2), 'o');
+%             end
+            
             %Calculate distance Matrix from centroids and invalid area
             distanceMatrixNSCsBrdU = classifyByRegionAndCalculateDistanceMatrix(centroidsNSCsBrdU,invalidArea,H,W);
             
-            [maxDist, indicesRow] = max(oldPointsInfo.distanceMatrix);
+            [maxDist, indicesRow] = max(oldPointsInfo.distanceMatrix, [], 2);
             
             [~, indicesCol] = max(maxDist);
             
 %             %Check if the extreme points are the good ones
-%             plot(oldPointsInfo.Centroids(:, 1), oldPointsInfo.Centroids(:, 2), '*')
+%             figure; plot(oldPointsInfo.Centroids(:, 1), oldPointsInfo.Centroids(:, 2), '*')
 %             hold on;plot(oldPointsInfo.Centroids(indicesCol, 1), oldPointsInfo.Centroids(indicesCol, 2), 'o')
 %             hold on;plot(oldPointsInfo.Centroids(indicesRow(indicesCol), 1), oldPointsInfo.Centroids(indicesRow(indicesCol), 2), 'o')
             cornerCentroidsIndices = [indicesCol indicesRow(indicesCol)];
-            centroidsBrdUWithCorners = vertcat(centroidsNSCsBrdU, oldPointsInfo.Centroids([indicesCol indicesRow(indicesCol)], :));
+            centroidsBrdUWithCorners = unique(vertcat(centroidsNSCsBrdU, oldPointsInfo.Centroids([indicesCol indicesRow(indicesCol)], :)), 'rows');
+            
             distanceMatrixWithCornersNSCsBrdU = classifyByRegionAndCalculateDistanceMatrix(centroidsBrdUWithCorners, invalidArea, H, W);
-            thisSplittedPath=splittedPath{nFile};
-
-
+            
             if ~isdir(['resultsByAnimal\NSCs BrdU\distanceMatrix\' month '\' thisSplittedPath{end-1} '\'])
                 mkdir(['resultsByAnimal\NSCs BrdU\distanceMatrix\' month '\' thisSplittedPath{end-1} '\']);
             end
-            save(['resultsByAnimal\NSCs BrdU\distanceMatrix\'  month '\' thisSplittedPath{end-1} '\' imgName '.mat'], 'distanceMatrixNSCsBrdU', 'distanceMatrixWithCornersNSCsBrdU','centroidsNSCsBrdU', 'centroidsBrdUWithCorners', 'cornerCentroidsIndices')
+            save(outputFile, 'distanceMatrixNSCsBrdU', 'distanceMatrixWithCornersNSCsBrdU','centroidsNSCsBrdU', 'centroidsBrdUWithCorners', 'cornerCentroidsIndices')
         end
     end
 
